1b42528510c15b64cf9a899412701ef1c21a346d
Nikos Pavlou <nicos9.pavlou@gmail.com>
2025-03-22 02:25:58 +0200

    log

--- ChatBotAPI/Clients/AIProviderClientService.cs
+++ ChatBotAPI/Clients/AIProviderClientService.cs
@@ -1,19 +1,33 @@
 ﻿using AIProviderAPI.Protos;
+using Grpc.Core;
 
 namespace ChatBotAPI.Clients
 {
     public class AIProviderClientService : IAIProviderClientService
     {
         public AIProviderService.AIProviderServiceClient _aiProviderClient;
+        public ILogger<AIProviderClientService> _logger;
 
-        public AIProviderClientService(AIProviderService.AIProviderServiceClient aiProviderClient)
+        public AIProviderClientService(
+            AIProviderService.AIProviderServiceClient aiProviderClient,
+            ILogger<AIProviderClientService> logger)
         {
             _aiProviderClient = aiProviderClient;
+            _logger = logger;
         }
 
         public async Task<AIResponse> PromptAIAsync(AIRequest request)
         {
-            return await _aiProviderClient.PromptAIAsync(request);
+            try
+            {
+                return await _aiProviderClient.PromptAIAsync(request);
+            }
+     
+            catch (RpcException e)
+{
+                _logger.LogError($"gRPC Error: {e.StatusCode}, {e.Message}");
+                throw;
+            }
         }
     }
 }
